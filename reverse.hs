58f718770994a43ff5faba0b022a1f86fab12284
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2019-01-09 01:06:03 +0200

    Add take, replicate, reverse

--- reverse.hs
+++ reverse.hs
@@ -1,3 +1,9 @@
-reverse' :: [Int] -> [Int]
+reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse xs ++ [x]
+
+reverse'' :: [a] -> [a]
+reverse'' xs = loop xs []
+    where
+        loop [] ys = ys
+        loop (x:xs) ys = loop xs (x:ys)
